a3cddb1573992c3ae0137973077fdc56073e253f
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-08-12 11:51:48 -0700

    Update wiki.js

--- files/wiki.js
+++ files/wiki.js
@@ -4,6 +4,6 @@ function makeWiki() {
 
 function wikiContent(title, text, images) {
   let wiki = '<a href="https://mariocraft987.github.io/wiki-maker/"><img src="https://mariocraft987.github.io/wiki-maker/files/title.svg"></a>';
-  wiki += '<input placeholder="Search WikiMaker"><button>Search</button>';
+  wiki += '<div style="translate: 8px -8px;"><input placeholder="Search WikiMaker"><button>Search</button></div>';
   return wiki;
 }
