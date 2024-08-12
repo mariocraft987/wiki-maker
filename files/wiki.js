9a1dcd352aa49316787453f9b33c6bf9d3c44ab4
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-08-12 11:38:01 -0700

    Create wiki.js

---
+++ files/wiki.js
@@ -0,0 +1,7 @@
+function makeWiki() {
+  document.getElementById('output').innerHTML = wikiContent();
+}
+
+function wikiContent(title, text, images) {
+  let wiki = '<a href="https://mariocraft987.github.io/wiki-maker/"><img src="https://mariocraft987.github.io/wiki-maker/files/title.svg"></a>'
+}
